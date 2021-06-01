299412607d07c9e1c674723d7461383e718050a3
seyituludag0 <seyituludag0@gmail.com>
2021-06-02 02:05:05 +0300

    SchoolDao, SchoolService, SchoolManager SchoolsController classes added

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SchoolsController.java
@@ -0,0 +1,39 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.SchoolService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.School;
+
+@RestController
+@RequestMapping("/api/schools")
+public class SchoolsController {
+
+	private SchoolService schoolService;
+	
+	@Autowired
+	public SchoolsController(SchoolService schoolService) {
+		super();
+		this.schoolService = schoolService;
+	}
+	
+	@GetMapping("/getAll")
+	public DataResult<List<School>> getAll(){
+		return this.schoolService.getAll();
+	}
+	
+
+	@PostMapping("/add")
+	public ResponseEntity<?> add(@RequestBody School school){
+		return ResponseEntity.ok(this.schoolService.add(school));
+	}
+}
