0481d6e459cd98cb18e5a6a142823c58a0653081
seyituludag0 <seyituludag0@gmail.com>
2021-06-03 12:32:51 +0300

    SocialMediaDao, SocialMediaService, SocialMediaManager
    SocialMediasController classes added

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/SocialMediasController.java
@@ -0,0 +1,41 @@
+package kodlamaio.hrms.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.SocialMediaService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.SocialMedia;
+
+@RestController
+@RequestMapping("/api/socialMedias")
+public class SocialMediasController {
+
+	private SocialMediaService socialMediaService;
+
+	@Autowired
+	public SocialMediasController(SocialMediaService socialMediaService) {
+		super();
+		this.socialMediaService = socialMediaService;
+	}
+	
+	
+
+	@GetMapping("/getAll")
+	public DataResult<List<SocialMedia>> getAll(){
+		return this.socialMediaService.getAll();
+	}
+	
+
+	@PostMapping("/add")
+	public ResponseEntity<?> add(@RequestBody SocialMedia socialMedia){
+		return ResponseEntity.ok(this.socialMediaService.add(socialMedia));
+	}
+}
