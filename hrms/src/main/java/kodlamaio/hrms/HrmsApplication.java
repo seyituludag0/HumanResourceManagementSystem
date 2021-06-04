2f48f46817e657060cbeec3790ee7acab97ad4f8
seyituludag0 <seyituludag0@gmail.com>
2021-06-04 12:37:03 +0300

    Refactoring

--- hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
+++ hrms/src/main/java/kodlamaio/hrms/HrmsApplication.java
@@ -1,6 +1,6 @@
 package kodlamaio.hrms;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -25,8 +25,5 @@ public class HrmsApplication {
 				.apis(RequestHandlerSelectors.basePackage("kodlamaio.hrms")).build();
 	}
 	
-	@Bean
-	public ModelMapper modelMapper() {
-		return new ModelMapper();
-	}
+	
 }
---
+++ hrms/src/main/java/kodlamaio/hrms/configurations/AppConfiguration.java
@@ -0,0 +1,15 @@
+package kodlamaio.hrms.configurations;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+}
