f3b1dd49bb77c38934d44f811dc4f6027f20e884
seyituludag0 <seyituludag0@gmail.com>
2021-07-09 02:23:26 +0300

    Refactoring

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/AbilityCandidatesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/AbilityCandidatesController.java
@@ -1,7 +1,5 @@
 package kodlamaio.hrms.api.controllers;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
