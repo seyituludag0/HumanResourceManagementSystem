da054a650c1ef60ddb0094f2bb548bba22c46fe4
seyituludag0 <seyituludag0@gmail.com>
2021-07-09 02:38:48 +0300

    LanguageCandidatesController refactoring

--- hrms/src/main/java/kodlamaio/hrms/api/controllers/LanguageCandidatesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controllers/LanguageCandidatesController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import kodlamaio.hrms.business.abstracts.LanguageCandidateService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
 import kodlamaio.hrms.core.utilities.results.Result;
